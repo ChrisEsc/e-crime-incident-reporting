456b9f5e1d09024b2246ec215af778ed23f81133
Christian Esclamado <chrisesclamado@gmail.com>
2023-08-15 15:12:51 +0800

    removed unnecessary comments and whitespaces, updated syntax of some code

--- application/views/reports_receiver/index.php
+++ application/views/reports_receiver/index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<form action="reports_receiver/receivereports" method="post" target="_blank">
-  Name 		: <input type="text" name="name"><br>
-  Details 	: <input type="text" name="details"><br>
-  Latitude 	: <input type="text" name="lat"><br>
-  Longitude : <input type="text" name="lng"><br>
-  <input type="submit" value="Submit">
-</form>
-
+  <form action="reports_receiver/receivereports" method="post" target="_blank">
+    Name 		: <input type="text" name="name"><br>
+    Details 	: <input type="text" name="details"><br>
+    Latitude 	: <input type="text" name="lat"><br>
+    Longitude : <input type="text" name="lng"><br>
+    <input type="submit" value="Submit">
+  </form>
 </body>
 </html>
